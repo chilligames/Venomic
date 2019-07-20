2b66507afeeaedb235958509e09ed7c0c9241590
Hossynkoala <42633146+Hossynkoala@users.noreply.github.com>
2019-07-20 20:49:16 +0430

    Menu->Btn_play add

--- Prefabs/Menu/BTN_Play/BTN_Play.cs
+++ Prefabs/Menu/BTN_Play/BTN_Play.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.EventSystems;
 
 namespace Script_game.menu
 {
 
-    public class BTN_Play : Menu
+    public class BTN_Play : Menu, IPointerEnterHandler, IPointerExitHandler
     {
+
 
         public RawImage Dot_shape;
         public Transform Place_Dots;
         public float Speed_dot;
         public float Degress_dot_internal;
         public Vector3[] Pos_dots;
+
         Vector3[] pos_dots_internal;
         RawImage[] Dots = new RawImage[6];
         RawImage[] Dots_internal = new RawImage[6];
@@ -22,10 +24,14 @@ namespace Script_game.menu
         public LineRenderer[] Lines;
         Envorment_dot Dot_envorment = new Envorment_dot();
 
+        int Active_BTN_play;
+
+        Vector3[] Frist_location;
         private void Start()
         {
 
-
+            Frist_location = new Vector3[6] { Pos_dots[0], Pos_dots[1], Pos_dots[2], Pos_dots[3], Pos_dots[4], Pos_dots[5] };
+            print(Frist_location[0]);
             for (int i = 0; i < Pos_dots.Length; i++)
             {
                 Dots[i] = Instantiate(Dot_shape, Place_Dots);
@@ -37,6 +43,7 @@ namespace Script_game.menu
 
         private void Update()
         {
+            print(Frist_location[0]);
             Dot_envorment.Instant_Dot_Envorment(Dots, Pos_dots, Speed_dot);
             Dot_envorment.Instant_Dot_Envorment(Dots_internal, pos_dots_internal, Speed_dot / 3);
 
@@ -45,21 +52,75 @@ namespace Script_game.menu
                 Lines[0].SetPosition(i, Dots[i].transform.position);
                 Lines[1].SetPosition(i, Dots_internal[i].transform.position);
             }
+
         }
-
-
-
-        public class Envorment_dot
+        /// <summary>
+        /// animation Enter
+        /// </summary>
+        /// <param name="eventData"></param>
+        public void OnPointerEnter(PointerEventData eventData)
         {
 
-            public void Instant_Dot_Envorment(RawImage[] Dot, Vector3[] Pos_dots, float Speed)
+            while (true)
             {
-                for (int i = 0; i < Dot.Length; i++)
+                if (Vector3.Distance(Pos_dots[0], Vector3.zero) > 0)
                 {
-                    Dot[i].transform.localPosition = Vector3.MoveTowards(Dot[i].transform.localPosition, Pos_dots[i], Speed);
+                    for (int i = 0; i < Pos_dots.Length; i++)
+                    {
+                        Pos_dots[i] = Vector3.MoveTowards(Pos_dots[i], Vector3.zero, 0.1f);
+                    }
                 }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// animation EXit
+        /// </summary>
+        /// <param name="eventData"></param>
+        public void OnPointerExit(PointerEventData eventData)
+        {
+
+            while (true)
+            {
+
+                if (Vector3.Distance(Pos_dots[0], Vector3.zero) < Vector3.Distance(Frist_location[0], Vector3.zero))
+                {
+                    for (int i = 0; i < Pos_dots.Length; i++)
+                    {
+                        Pos_dots[i] = Vector3.MoveTowards(Pos_dots[i], Frist_location[i], 0.1f);
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+
+
+        }
+
+    }
+
+
+
+    public class Envorment_dot
+    {
+
+        public void Instant_Dot_Envorment(RawImage[] Dot, Vector3[] Pos_dots, float Speed)
+        {
+            for (int i = 0; i < Dot.Length; i++)
+            {
+                Dot[i].transform.localPosition = Vector3.MoveTowards(Dot[i].transform.localPosition, Pos_dots[i], Speed);
             }
         }
     }
-
 }
+
