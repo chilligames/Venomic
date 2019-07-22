652757c60432489327b7bae97940583d246187e7
Hossynkoala <42633146+Hossynkoala@users.noreply.github.com>
2019-07-22 09:19:11 +0430

    Menu->BTN_play ->line Snap creat

--- Prefabs/Menu/BTN_Play/BTN_Play.cs
+++ Prefabs/Menu/BTN_Play/BTN_Play.cs
@@ -9,6 +9,7 @@ namespace Script_game.menu
 
     public class BTN_Play : Menu, IPointerEnterHandler, IPointerExitHandler, IDragHandler
     {
+
         [Header("Envorment")]
         Envorment_dot Dot_envorment = new Envorment_dot();
         public RawImage Dot_shape;
@@ -22,8 +23,9 @@ namespace Script_game.menu
         Vector3[] Frist_Pos_internal_dot = new Vector3[6];
         RawImage[] Dots = new RawImage[6];
         RawImage[] Dots_internal = new RawImage[6];
-        [Space(30f)]
+
         [Header("Lines")]
+        [Space(30)]
 
         public LineRenderer line_raw;
         public Transform Place_line_instatnt;
@@ -31,15 +33,27 @@ namespace Script_game.menu
         Vector3[] Target_line = new Vector3[10];
         Vector3[] Target_line_2 = new Vector3[10];
 
+        [Header("Connect")]
+        [Space(30)]
+        public LineRenderer Line_raw_Snap;
+        public Transform Place_line_Snap;
+        public LineRenderer[] Line_Snap = new LineRenderer[6];
+
         private void Start()
         {
 
             pos_dots_internal = new Vector3[] { new Vector2(Pos_dots[0].x, Pos_dots[0].y - Degress_dot_internal - 0.2f), new Vector2(Pos_dots[1].x - Degress_dot_internal, Pos_dots[1].y - Degress_dot_internal), new Vector2(Pos_dots[2].x - Degress_dot_internal, Pos_dots[2].y + Degress_dot_internal), new Vector2(Pos_dots[3].x, Pos_dots[3].y + Degress_dot_internal + 0.2f), new Vector2(Pos_dots[4].x + Degress_dot_internal, Pos_dots[4].y + Degress_dot_internal), new Vector2(Pos_dots[5].x + Degress_dot_internal, Pos_dots[5].y - Degress_dot_internal) };
             for (int i = 0; i < Pos_dots.Length; i++)
             {
+                Line_Snap[i] = Instantiate(Line_raw_Snap, Place_line_Snap);
+            }
+
+            for (int i = 0; i < Pos_dots.Length; i++)
+            {
                 Dots[i] = Instantiate(Dot_shape, Place_Dots);
                 Dots_internal[i] = Instantiate(Dot_shape, Place_Dots);
             }
+
             for (int i = 0; i < Pos_dots.Length; i++)
             {
                 Frist_Pos[i] = Pos_dots[i];
@@ -61,7 +75,11 @@ namespace Script_game.menu
                 Lines[0].SetPosition(i, Dots[i].transform.position);
                 Lines[1].SetPosition(i, Dots_internal[i].transform.position);
             }
-
+            for (int i = 0; i < Line_Snap.Length; i++)
+            {
+                Line_Snap[i].SetPosition(0, Vector3.MoveTowards(Line_Snap[i].GetPosition(0), pos_dots_internal[i], 0.01f));
+                Line_Snap[i].SetPosition(1, Vector3.MoveTowards(Line_Snap[i].GetPosition(1), Pos_dots[i], 0.03f));
+            }
         }
 
 
