b9c70c8a9e7fb8a539c14efda2de863bb8449f84
Hossynkoala <42633146+Hossynkoala@users.noreply.github.com>
2019-07-22 20:15:50 +0430

    Game_play ->BTN_sample add

---
+++ Prefabs/Game_play/BTN_Sampel/BTN_sample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTN_sample : MonoBehaviour
+{
+
+
+    public static int Tap_count;
+    public static int Sampe_count;
+
+
+
+}
